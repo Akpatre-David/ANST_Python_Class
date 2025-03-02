5e7a16440472ccc58699f679d55c7c3c28a1d2d4
Akpatre-David <akpatredavid17@gmail.com>
2025-03-02 22:51:05 +0100

    corrected the logical operator

---
+++ logicalOperator.py
@@ -0,0 +1,11 @@
+f_name = input("Enter First Name: ")
+l_name = input("Enter Last  Name: ")
+age = int(input("Enter Age: "))
+
+# print("Welcome" +" "+ f_name)
+
+if age<=30:
+    print(f"Welcome {f_name}, Hello young star")
+    
+else:
+    print(f"Hello {f_name},unauthorized")
