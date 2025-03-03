77b1135532677edff988732622f599322e703e59
Akpatre-David <akpatredavid17@gmail.com>
2025-03-03 19:33:52 +0100

    wip

--- logicalOperator.py
+++ logicalOperator.py
@@ -2,10 +2,11 @@ f_name = input("Enter First Name: ")
 l_name = input("Enter Last  Name: ")
 age = int(input("Enter Age: "))
 
-# print("Welcome" +" "+ f_name)
-
-if age<=30:
-    print(f"Welcome {f_name}, Hello young star")
+if age <= 17:
+    print(f"Welcome {f_name} {l_name}, Hello young star")
+    
+elif age >=100:
+    print(f"Hello {f_name} {l_name},too old")
     
 else:
-    print(f"Hello {f_name},unauthorized")
+      print(f"Hello {f_name} {l_name},You Are welcome")
